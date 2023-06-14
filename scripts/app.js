37ef4a61994a4f3ff50029840fc2bca8e5d8699a
Jenni Sol <94581689+JenniferBellott@users.noreply.github.com>
2023-06-14 14:04:36 -0300

    cambios nuevos

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,1 @@
-
-    alert("Bienvenidos a mi página!!");
-    const $btnSignIn= document.querySelector('.sign-in-btn'),
-    $btnSignUp = document.querySelector('.sign-up-btn'),  
-    $signUp = document.querySelector('.sign-up'),
-    $signIn  = document.querySelector('.sign-in');
-
-document.addEventListener('click', e => {
-  if (e.target === $btnSignIn || e.target === $btnSignUp) {
-      $signIn.classList.toggle('active');
-      $signUp.classList.toggle('active')
-  }
-});
+  alert("Bienvenidos a mi página!!");
