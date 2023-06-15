069e5edfd1848853618119268030ef72f0b9f396
Jenni Sol <94581689+JenniferBellott@users.noreply.github.com>
2023-06-15 11:33:56 -0300

    retoques finales

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,14 @@
- alert("Bienvenidos a mi página!!");
+ alert("Gracias por visitar Durlocraks!!");
+
+ function validarFormulario(evento) {
+    evento.preventDefault();
+    var datos = document.getElementById("nombre", "apellido", "telefono", "email",).value;
+    if(datos.length == 0) {
+    alert('No has escrito nada');
+    return;
+    }
+    
+    alert("Le enviaremos un mail con la informacion requerida");
+    document.formulario.submit();
+}
 
